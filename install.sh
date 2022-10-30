ab863a2b6bfb96c82da44b0b820a41955ed9198a
André Schemaitat <a.schemaitat@gmail.com>
2022-10-31 00:25:45 +0100

    Add absolute path for install scripts.

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 sudo apt-get update && export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -y install --no-install-recommends vim
 
-cp .vimrc $HOME/.vimrc
+cp $SCRIPT_PATH/.vimrc $HOME/.vimrc
 
-./zsh-in-docker.sh \
+$SCRIPT_PATH/zsh-in-docker.sh \
     -p git -p git-auto-fetch \
     -p https://github.com/zsh-users/zsh-autosuggestions \
     -p https://github.com/zsh-users/zsh-completions \
@@ -15,4 +17,4 @@ cp .vimrc $HOME/.vimrc
     -a 'HYPHEN_INSENSITIVE="true"' \
     -a "export TERM=xterm-256color"
 
-./zshrc-personal.sh
+$SCRIPT_PATH/zshrc-personal.sh
